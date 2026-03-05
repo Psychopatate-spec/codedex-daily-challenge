5118302674a6ddfc5ef3b123709ecadcb6e93d80
Ilysk <iklassgames@gmail.com>
2026-03-05 18:18:24 +0000

    Day 4

---
+++ 4-dompierMusic.js
@@ -0,0 +1,27 @@
+function dompierMusic(switches) {
+  let decimalNumber
+  let music = []
+  for (let i = 0; i < switches.length; i++) {
+    decimalNumber = parseInt(switches[i], 2)
+    if (decimalNumber == 261) {
+      music.push("C4")
+    } else if (decimalNumber == 294) {
+      music.push("D4")
+    } else if (decimalNumber == 329) {
+      music.push("E4")
+    } else if (decimalNumber == 349) {
+      music.push("F4")
+    } else if (decimalNumber == 392) {
+      music.push("G4")
+    } else if (decimalNumber == 440) {
+      music.push("A4")
+    } else if (decimalNumber == 494) {
+      music.push("B4")
+    } else if (decimalNumber == 523) {
+      music.push("C4")
+    } else if (decimalNumber == 0) {
+      music.push("REST")
+    }
+  }
+  return music
+}
