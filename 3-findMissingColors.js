86de4c0abbf9b4b0b896d4baec67fde75c7760f1
Ilysk <iklassgames@gmail.com>
2026-03-05 18:19:27 +0000

    Refactor findMissingColors function

--- 3-findMissingColors.js
+++ 3-findMissingColors.js
@@ -1,9 +1,7 @@
 function findMissingColors(grid) {
-
   let colors = ["🟥", "🟧", "🟨", "🟩", "🟦", "🟪", "🟫"]
   let presentColors = []
   let missingColors = []
-
   //Checks for every color present in the grid
   for (let i = 0; i < 7; i++) {
     for (let j = 0; j < 7; j++) {
@@ -12,13 +10,11 @@ function findMissingColors(grid) {
       }
     }
   }
-
   //Checks for the missing colors in the present colors
   for (i = 0; i < colors.length; i++) {
     if (!presentColors.includes(colors[i])) {
       missingColors.push(colors[i])
     }
   }
-
   return missingColors
 }
